2a9f7170d99fdd8ced9e6b5fb0567d588e4c0008
Igor Tuag <igortuag@gmail.com>
2020-10-02 07:50:37 -0300

    Animacao ao scroll

--- 0311-scroll-suave-links-internos/script.js
+++ 0311-scroll-suave-links-internos/script.js
@@ -67,28 +67,3 @@ const initScrollSuave = () => {
   }
 };
 initScrollSuave();
-
-const initAnimacaoScroll = () => {
-  const sections = document.querySelectorAll(`.js-scroll`);
-
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-
-    const animalScroll = () => {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else {
-          section.classList.remove("ativo");
-        }
-      });
-    };
-
-    animalScroll();
-
-    window.addEventListener("scroll", animalScroll);
-  }
-};
-initAnimacaoScroll();
