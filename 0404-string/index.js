e3f511899fbc422409cff1802f674960702e6a6e
Igor Tuag <igortuag@gmail.com>
2020-10-04 16:22:06 -0300

    [0404] string 3

--- 0404-string/index.js
+++ 0404-string/index.js
@@ -1,76 +1,56 @@
-// const comida = "Pizza";
-// const agua = new String("Agua");
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-// console.log(comida.length);
-// console.log(agua.length);
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
 
-// const frase = "A melhor comida";
+let totalTransacoes = 0
+transacoes.forEach((el) => {
+  totalTransacoes += +el.valor.replace('R$ ', '') 
+})
+console.log(totalTransacoes)
 
-// console.log(frase[frase.length - 1])
-// console.log(frase.charAt(frase.length - 1))
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+console.log(transportes.split(';'))
 
-// const frase = "A melhor linguagem é ";
-// const linguagem = "Javascript";
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+console.log(html.split('span').join('a'))
 
-// const fraseFinal = frase.concat(linguagem, '!', '!')
-// console.log(fraseFinal)
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+console.log(frase[frase.length - 1])
 
-const fruta = "Banana";
-const listaFrutas = "Melancia, Banana, Laranja";
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let totalTaxas = 0
+transacoes2.forEach(el => {
+  totalTaxas += el.toLocaleLowerCase().includes('taxa') ? 1 : 0;
+})
 
-// console.log(listaFrutas.includes(fruta));
-// console.log(listaFrutas.includes(fruta, 14));
-// console.log(fruta.includes(listaFrutas));
-
-// console.log(fruta.startsWith("Ba"));
-// console.log(fruta.endsWith("na"));
-
-const transacao1 = "Deposito de cliente";
-const transacao2 = "Deposito de fornecedor";
-const transacao3 = "Taxa de camisas";
-
-console.log(transacao3.slice(3));
-console.log(transacao3.slice(0, -3));
-console.log(transacao3.slice(-5));
-
-console.log(fruta.indexOf("a"));
-console.log(fruta.lastIndexOf("na"));
-
-const preco = "R$ 99,00";
-
-const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
-
-// listaPrecos.forEach((el) => {
-//   console.log(el.padStart(10, "-"));
-// });
-
-// const listaPedidos = ['1', '2', '3', '4', '5', '6', '22', '354', '485'];
-// listaPedidos.forEach((el) => {
-//   console.log(el.padStart(6, '0'))
-// })
-
-frase2 = "TA";
-
-//console.log(frase2.repeat(5))
-
-let listaItens = "Camisa Bonés Calças Bermudas Vestidos Saias";
-
-listaItens = listaItens.replace(/[ ]+/g, ", ");
-
-const arrayLista = listaItens.split(", ");
-
-// console.log(arrayLista)
-
-const htmlText = "<div>O melhor item</div><div>A melhor lista</div>";
-const htmlArray = htmlText.split("div");
-const novoHtml = htmlArray.join('section')
-// console.log(htmlText);
-// console.log(htmlArray);
-console.log(novoHtml);
-
-const frutasArray = ['Banana', 'Melancia', 'Laranja']
-
-const valor = ' R$ 23.00  '
-console.log(valor.trim())
-console.log(valor.trimStart())
-console.log(valor.trimEnd())
+console.log(totalTaxas)
