f84a6836524b4ebcc4d297b1d4044c5b35411024
Igor Tuag <igortuag@gmail.com>
2020-10-12 13:50:47 -0300

    [0509] - Menu mobile

--- js/modules/outsideClick.js
+++ js/modules/outsideClick.js
@@ -3,17 +3,19 @@ export default function outsideClick(element, events, callback) {
   const outside = "data-outside";
 
   if (!element.hasAttribute(outside)) {
-    events.forEach(userEvent => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    })
+    events.forEach((userEvent) => {
+      setTimeout(() => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      });
+    });
     element.setAttribute(outside, "");
   }
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
       element.removeAttribute(outside);
-      events.forEach(userEvent => {
+      events.forEach((userEvent) => {
         html.removeEventListener(userEvent, handleOutsideClick);
-      })
+      });
       callback();
     }
   }
