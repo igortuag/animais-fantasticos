4eab8633f64f8a1b70b6f0a55b2ba12d27b807b2
Igor Tuag <igortuag@gmail.com>
2020-10-30 19:57:37 -0300

    Fixed caminho da api

--- js/script.js
+++ js/script.js
@@ -44,6 +44,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
